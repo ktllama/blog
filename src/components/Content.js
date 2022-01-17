d86c50ddffc87359548c16a5f247157b893078e0
Katie <costantinkp@gmail.com>
2022-01-17 11:10:50 -0500

    header, content, footer of blog components created

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Content = () => {
+    return (
+        <main>
+            <h2>this is the main content</h2>
+        </main>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>Blog</h1> 
+        </header>
+    )
+}
