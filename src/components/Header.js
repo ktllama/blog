6a3d0e9957572e947e2011c265c4540bf81cdd2c
Katie <costantinkp@gmail.com>
2022-01-17 11:31:42 -0500

    external style sheet applied

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,10 @@ import React from 'react'
 
 export const Header = () => {
 
-    const headerStyle = {
-        backgroundColor: 'salmon',
-        color:'white'
-    };
+
 
     return (
-        <header style={headerStyle}>
+        <header>
             <h1>Blog</h1> 
         </header>
     )
