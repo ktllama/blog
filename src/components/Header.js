8c1267db093810350a22254d9e576f7c39365634
Katie <costantinkp@gmail.com>
2022-01-17 11:19:26 -0500

    inline style in header practice

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 export const Header = () => {
+
+    const headerStyle = {
+        backgroundColor: 'salmon',
+        color:'white'
+    };
+
     return (
-        <header>
+        <header style={headerStyle}>
             <h1>Blog</h1> 
         </header>
     )
