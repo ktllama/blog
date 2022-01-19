23351e44b1af27504a51e5b90d5eac8a5e78a972
Katie <costantinkp@gmail.com>
2022-01-19 14:11:12 -0500

    wrapped ul in ternary so if list is empty it will say list is empty

--- src/components/Content.js
+++ src/components/Content.js
@@ -35,25 +35,29 @@ export const Content = () => {
 
     return (
         <main>
-        <ul>
-            {items.map((item) => (
-                <li key={item.id} className="item">
-                    <input
-                        type="checkbox"
-                        onChange={() => handleCheck(item.id)}
-                        checked={item.checked}
-                    />
-                <label
-                    style={item.checked ? { textDecoration: 'line-through'} : null}
-                    onDoubleClick={() => handleCheck(item.id)}
-                >{item.item}</label>
-                <FaTrashAlt 
-                    onClick={() => handleDelete(item.id)}
-                    role="button" 
-                    tabIndex="0" />
-                </li>
-            ))}
-        </ul>
+            {items.length ? (
+                <ul>
+                    {items.map((item) => (
+                        <li key={item.id} className="item">
+                            <input
+                                type="checkbox"
+                                onChange={() => handleCheck(item.id)}
+                                checked={item.checked}
+                            />
+                        <label
+                            style={item.checked ? { textDecoration: 'line-through'} : null}
+                            onDoubleClick={() => handleCheck(item.id)}
+                        >{item.item}</label>
+                        <FaTrashAlt 
+                            onClick={() => handleDelete(item.id)}
+                            role="button" 
+                            tabIndex="0" />
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p style={{marginTop : '2rem' }}> Your list is empty. </p>
+            )}
 
         </main>
     )
