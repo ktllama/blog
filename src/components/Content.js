be67c144380acc899971b20413ec399cd4cbdbf2
Katie <costantinkp@gmail.com>
2022-01-18 20:30:02 -0500

    adding useState

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import { useState } from 'react'
 
 export const Content = () => {
+    const [name, setName] = useState('katie');
+
+    const handleNameChange = () =>{
+        const names=['katie', 'molly', 'mary', 'kiersten'];
+        const int = Math.floor(Math.random()*4);
+        setName(names[int]);
+    }
+    //how you make random selections- *4 because there are 4 names
 
     const handleClick = () => {
         console.log('you clicked it');
@@ -16,9 +24,12 @@ export const Content = () => {
 
     return (
         <main>
-            <h2>this is the main content</h2>
+        <h1>Who is driving?</h1>
+            <h2>{name} is driving tonight!</h2>
+            <button onClick={handleNameChange}>click to see</button>
+            <br />
+            <h3> random buttons in console </h3>
             <p onDoubleClick={handleClick}>double click</p>
-            <button onClick={handleClick}>click me</button>
             <button onClick={() => handleClick2('katie')}>name</button>
             {/* this function is not called immediatley - called on the click because its wrapped in an annon function- making it a callback that only runs when clickes */}
             <button onClick={(e) => handleClick3(e)}>event</button>
