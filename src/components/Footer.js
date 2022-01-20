5521808c945423f6c775bc122eab99fa4b9be3a0
Katie <costantinkp@gmail.com>
2022-01-20 17:11:15 -0500

    footer counting list items

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export const Footer = () => {
+export const Footer = ({ length }) => {
 
     const today = new Date();
 
     return (
         <footer>
-            <p>Copyright &copy; {today.getFullYear()}</p>
+            <p>{length} list { length === 1 ? "item" : "items" }</p>
         </footer>
     )
 }
