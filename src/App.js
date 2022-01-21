3bb4b390d237cee7e4b36372f9737e99aea8b656
Katie <costantinkp@gmail.com>
2022-01-21 18:21:49 -0500

    remove default state- set items state to local storage

--- src/App.js
+++ src/App.js
@@ -6,23 +6,7 @@ import './App.css';
 import { AddItem } from './components/AddItem';
 
 function App() {
-  const [items, setItems] = useState([
-    {
-        id: 1,
-        checked: false,
-        item: "One half pound bag of Cocoa Covered Almonds Unsalted"
-    },
-    {
-        id: 2,
-        checked: false,
-        item: "Item 2"
-    },
-    {
-        id: 3,
-        checked: false,
-        item: "Item 3"
-    }
-  ]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
 
   const[newItem, setNewItem] = useState('');
 
